4030c14157ec60fbdcffd2e19a5b870a6b14455b
Sóré Aladelusi <sore.aladelusi@yahoo.com>
2022-03-03 18:31:08 +0000

    added focus listener and add and delete post func

--- components/profile.js
+++ components/profile.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text, FlatList} from 'react-native';
+import {View, Text, FlatList, Button, TextInput} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { ActivityIndicator } from 'react-native-paper';
 
@@ -14,9 +14,22 @@ class ProfileScreen extends Component {
     }
   }
 
+  checkLoggedIn = async () => {
+    const value = await AsyncStorage.getItem('@session_token');
+    if (value == null) {
+        this.props.navigation.navigate('Login');
+    }
+  };
+
   componentDidMount() {
+    this.unsubscribe = this.props.navigation.addListener('focus', () => {
     this.getProfile();
-  }
+    this.get_profile_image();
+    });
+
+  this.getProfile();
+  this.get_profile_image();
+}
 
   // checkLoggedIn = async () => {
   //   const value = await AsyncStorage.getItem('@session_token');
@@ -62,6 +75,64 @@ class ProfileScreen extends Component {
         })
   }
 
+  getAddPost = async (id) => {
+    const value = await AsyncStorage.getItem('@session_token');
+    return fetch("http://localhost:3333/api/1.0.0/user/" + id + "/post", {
+      method: 'post',
+      headers : {
+        'Content-Type' : 'appliaction/json',
+        'X_authorization' : value
+      }
+    })
+      .then((response) => {
+        if(response.status === 201){
+          console.log("Post Sent")
+        } else if(response.status === 401){
+          throw 'Unauthorised'
+        } else if(response.status === 404){
+          throw 'Not found'
+        } else if(response.status === 500){
+          throw 'Server error'
+        }
+      })
+      .catch((error) => {
+          console.log(error);
+      })
+  }
+
+  getDeletePost = async (id) => {
+    const value  = await AsyncStorage.getItem('@session_token');
+    return fetch("http://localhost:3333/api/1.0.0/user/" + id + "/post/" + id, {
+      method: 'delete',
+
+    })
+  }
+
+  get_profile_image = async (id) => {
+    const valuetoke = await AsyncStorage.getItem('@session_token');
+
+    const valueid  = await AsyncStorage.getItem('@session_id');
+    fetch("http://localhost:3333/api/1.0.0/user/" + id + "/photo", {
+      method: 'get',
+      headers: {
+        'X-Authorization': valuetoke
+      }
+    })
+    .then((res) => {
+      return res.blob();
+    })
+    .then((resBlob) => {
+      let data = URL.createObjectURL(resBlob);
+      this.setState({
+        photo: data,
+        isLoading: false
+      });
+    })
+    .catch((err) => {
+      console.log("error", err)
+    });
+  }
+
   render() {
 
     const {profile, isLoading} = this.state;
@@ -70,7 +141,13 @@ class ProfileScreen extends Component {
       return(<ActivityIndicator/>);
         } else {
       return (
-      <View><Text>{profile.first_name + ' '}{profile.last_name}</Text></View>
+      <View>
+        <Text>{profile.first_name + ' '}{profile.last_name}</Text>
+        <TextInput placeholder="Write a post here..."/>
+        <Button title = "Post" onPress={() => this.getAddPost()}/>
+        <Button title = "Delete" onPress={() => this.getDeletePost}/>
+        <Button title = "Update Profile Picture" onPress={() => this.props.navigation.navigate("Camera")}/>
+      </View>
 
       )}
     }
