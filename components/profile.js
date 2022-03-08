056b60637e6cb79d75e5de485699135c2fc99c71
Sóré Aladelusi <sore.aladelusi@yahoo.com>
2022-03-08 23:14:42 +0000

    add post sending to server

--- components/profile.js
+++ components/profile.js
@@ -11,7 +11,8 @@ class ProfileScreen extends Component {
       isLoading: true,
       profile: {},
       photo: null,
-      text: ""
+      text: "",
+      postData: ""
     }
   }
 
@@ -122,6 +123,35 @@ class ProfileScreen extends Component {
   //   })
   // }
 
+  getListofPosts = async () => {
+    const token = await AsyncStorage.getItem('@session_token');
+
+    const id = await AsyncStorage.getItem('@session_id');
+    fetch("http://localhost:3333/api/1.0.0/user/" + id + "/post", {
+      method: 'get',
+      headers: {
+        'Content-Type' : 'application/json',
+        'X-Authorization': token
+      }
+    })
+    .then((response) => {
+      if(response.status === 201){
+        console.log("Users Posts")
+      } else if(response.status === 401){
+        throw 'Unauthorised'
+      } else if(response.status === 403){
+        throw 'Can only view the posts of yourself or your friends'
+      } else if(response.status === 404){
+        throw 'Not Found'
+      } else if(response.status === 500){
+        throw 'Server error'
+      }
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+}
+
   getProfileImage = async () => {
     const token = await AsyncStorage.getItem('@session_token');
 
@@ -177,11 +207,18 @@ class ProfileScreen extends Component {
             }}
           />
         <Text>{profile.first_name + ' '}{profile.last_name}</Text>
-        <TextInput placeholder="Write a post here..." onPress={() => this.setState(text)} style={{borderRadius:4, padding:5, borderWidth:1, margin:5}}/>
+        <TextInput placeholder="Write a post here..." onChangeText={(text) => this.setState({text})} style={{borderRadius:4, padding:5, borderWidth:1, margin:5}}/>
         <Button title = "Post" onPress={() => this.getAddPost()}/>
-        <Button title = "Delete" onPress={() => this.getDeletePost}/>
-
+        <Button title = "Delete" onPress={() => this.getDeletePost()}/>
         <Button title = "Update Profile Picture" onPress={() => this.props.navigation.navigate("Camera")}/>
+        <FlatList
+          data = {this.state.postData}
+          renderItem = {({item}) => (
+          <View>
+            <Text>{item.timestamp}</Text>
+          </View>
+      )}
+        />
       </View>
       
       )}
